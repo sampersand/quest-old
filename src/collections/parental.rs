805a3a6f25fc4d1cc610aca4b423b14f3320304e
sampersand <westerhack@gmail.com>
2018-12-28 12:31:09 -0800

    updated parental

--- src/collections/parental.rs
+++ src/collections/parental.rs
@@ -4,12 +4,12 @@ use crate::object::{Object, r#type::Map as ObjMap};
 
 #[derive(Debug, Clone)]
 pub struct ParentalMap<M: Mapping = ObjMap> {
-	parent: Shared<Object>,
+	parent: Shared<dyn Mapping>,
 	map: M
 }
 
 impl<M: Mapping + Default> ParentalMap<M> {
-	pub fn new(parent: Shared<Object>) -> ParentalMap<M> {
+	pub fn new(parent: Shared<dyn Mapping>) -> ParentalMap<M> {
 		unimplemented!()
 	}
 }
@@ -33,33 +33,23 @@ impl Mapping for ParentalMap {
 			Some(obj)
 			// todo: make object a map
 		} else {
-			self.parent.read().a
+			self.parent.read().get(key)
 		}
 		// self.iter().find_map(|(k, v)| if k == key { Some(v) } else { None })
 	}
 
+	#[inline]
 	fn set(&mut self, key: Shared<Object>, val: Shared<Object>) -> Option<Shared<Object>> {
-		for i in 0..self.data.len() {
-			if self.data[i].0 == key {
-				let v = self.data[i].1.clone();
-				self.data[i] = (key, val);
-				return Some(v)
-			}
-		}
-		self.data.push((key, val));
-		None
+		self.map.set(key, val)
 	}
 
+	#[inline]
 	fn del(&mut self, key: &Shared<Object>) -> Option<Shared<Object>> {
-		for i in 0..self.data.len() {
-			if &self.data[i].0 == key {
-				return Some(self.data.swap_remove(i).1)
-			}
-		}
-		None
+		self.map.del(key)
 	}
 
+	#[inline]
 	fn has(&self, key: &Shared<Object>) -> bool {
-		self.keys().any(|k| k == key)
+		self.map.has(key) || self.parent.read().has(key)
 	}
 }
