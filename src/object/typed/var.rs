669afa9a36e3a290cd1f4f40fc7de7302d521996
sampersand <westerhack@gmail.com>
2018-12-28 16:20:39 -0800

    Doesn't work with Var, as that causes an infinite loop--what to do?

--- src/object/typed/var.rs
+++ src/object/typed/var.rs
@@ -8,12 +8,33 @@ pub struct Var(&'static str);
 
 impl Type for Var {
 	fn create_mapping() -> Shared<dyn Mapping> {
+		// lazy_static! {
+		// 	static ref PARENT: Shared<Object> = Shared::new({
+		// 		Object::new(crate::collections::Map::default())
+		// 	});
+		// }
+		// Shared::new(ParentalMap::new_default(PARENT.clone()))
+
 		lazy_static! {
-			static ref PARENT: Shared<Object> = Shared::new({
-				Object::new(crate::collections::Map::default())
-			});
+			static ref PARENT: Shared<Object> = Shared::new(Object::new({
+				fn at_text(args: &[&Shared<Object>]) -> crate::Result {
+					Ok(TypedObject::new_text(
+						args.get(0).ok_or_else(|| crate::Error::BadArgument("missing first arg", None))?
+						   .downcast_var().ok_or_else(|| crate::Error::BadArgument("@text called with non-var argument", Some(args[0].clone())))?
+						   .0.to_string()
+					).objectify())
+				}
+
+				let mut map = crate::collections::Map::default();
+				map.set(
+					TypedObject::new_var("@text").objectify(),
+					TypedObject::new_rustfn("Var.@text", at_text).objectify()
+				);
+				map
+			}));
 		}
 		Shared::new(ParentalMap::new_default(PARENT.clone()))
+
 	}
 }
 
