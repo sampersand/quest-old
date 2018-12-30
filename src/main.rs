2ecd70fef3b6db07b15858f02276b47f34d112e9
sampersand <westerhack@gmail.com>
2018-12-29 18:39:00 -0800

    parental mappings work for multiple layers up

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,16 @@ fn main() {
     let fifteen = 15i32.into_object();
 
     let mut parent = twenty.get_attr("@parent").unwrap();
-    parent.set_attr("()", parent.get_attr("*").unwrap());
+    let mut basic = parent.get_attr("@parent").unwrap();
+    // println!("{:#?}", parent.get_attr("@parent"));
+    basic.set_attr("()", parent.get_attr("*").unwrap());
+    // basic.set_attr("bool", basic.get_attr("@bool").unwrap());
 
     println!("{:?}", twenty.call_attr("()", &[&fifteen]));
     println!("{:?}", fifteen.call_attr("()", &[&twenty]));
     println!("{:?}", twenty.get_attr("()"));
     println!("{:?}", true.into_object().get_attr("()"));
+    println!("{:?}", true.into_object().call_attr("()", &[&fifteen]));
     // println!("{:?}", twenty.call_attr("+", &[&fifteen]));
     // println!("{:?}", twenty.call_attr("@bool", &[]));
 }
